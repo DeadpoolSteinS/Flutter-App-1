e149ac0a6cf0f62b5ff7c78b7c94cb9468510fa5
SteinS <novaldisago@gmail.com>
2022-07-24 17:27:38 +0700

    debug product not defined error

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const adminRouter = express.Router();
 const admin = require("../middlewares/admin");
-// const { Product } = require("../models/product");
+const { Product } = require("../models/product");
 // const Order = require("../models/order");
 // const { PromiseProvider } = require("mongoose");
 
@@ -25,14 +25,14 @@ adminRouter.post("/admin/add-product", admin, async (req, res) => {
 });
 
 // Get all your products
-// adminRouter.get("/admin/get-products", admin, async (req, res) => {
-//   try {
-//     const products = await Product.find({});
-//     res.json(products);
-//   } catch (e) {
-//     res.status(500).json({ error: e.message });
-//   }
-// });
+adminRouter.get("/admin/get-products", admin, async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 // // Delete the product
 // adminRouter.post("/admin/delete-product", admin, async (req, res) => {
